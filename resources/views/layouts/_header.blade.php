ba804075d68aea4d7d3bcef61a8251839844e8bf
huangcong <huangcong@daishujiankang.com>
2019-09-13 16:40:04 +0800

    完成登录和注册功能

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,29 @@
             </ul>
 
             <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+                {{-- 登录注册链接开始 --}}
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img
+                                src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                                class="img-responsive img-circle" width="30px" height="30px">
+                            {{ Auth::user()->nam }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a href="#" id="logout" class="dropdown-item"
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit()">退出登录</a>
+                            <form action="{{ route('logout') }}" id="logout-form" method="post" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+                {{-- 登录注册链接结束--}}
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
