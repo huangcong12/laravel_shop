62b6438bbc620fc86f78eb165362378b3f18a21b
huangcong <huangcong@daishujiankang.com>
2019-09-26 09:58:19 +0800

    调整数据库备份文件+1

--- back_admin_db.sh
+++ back_admin_db.sh
@@ -4,7 +4,7 @@
 source ./.env
 
 # 要备份的表
-tables="admin_menu admin_permissions admin_role_menu admin_role_primissions admin_role_users admin_roles admin_user_permissions admin_users"
+tables="admin_menu admin_permissions admin_role_menu admin_role_permissions admin_role_users admin_roles admin_user_permissions admin_users"
 
 # 执行备份
 mysqldump --host="${DB_HOST}" --port=${DB_PORT} --user="${DB_USERNAME}" --password="${DB_PASSWORD}" -t ${DB_DATABASE} ${tables} > database/admin.sql
